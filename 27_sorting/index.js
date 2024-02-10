de6336c2e94f3a26e35e388aaf0858325fbcf295
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2024-02-10 17:26:07 +0530

    bubble sort

---
+++ 27_sorting/index.js
@@ -0,0 +1,23 @@
+// --- Directions
+// Implement bubbleSort, selectionSort, and mergeSort
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let x = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = x;
+      }
+    }
+  }
+  return arr;
+}
+
+function selectionSort(arr) {}
+
+function mergeSort(arr) {}
+
+function merge(left, right) {}
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
