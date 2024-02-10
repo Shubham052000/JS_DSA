9ebff3b7eb3da424165450a975806ee134b82c52
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2024-02-10 21:32:03 +0530

    merge sort

--- 27_sorting/index.js
+++ 27_sorting/index.js
@@ -31,8 +31,29 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
 
-function merge(left, right) {}
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let res = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      res.push(left.shift());
+    } else {
+      res.push(right.shift());
+    }
+  }
+  res = [...res, ...left, ...right];
+  return res;
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
